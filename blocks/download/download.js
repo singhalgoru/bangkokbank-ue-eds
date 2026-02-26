792d0e82d0ba8b33693e1b32907f0be06818e975
SuchitraYaraguudri12 <syaragudri@adobe.com>
2026-02-26 13:18:11 +0530

    removed target

--- blocks/download/download.js
+++ blocks/download/download.js
@@ -2,19 +2,15 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    const [linkCell, targetCell] = row.children;
+    const [linkCell] = row.children;
     const anchor = linkCell?.querySelector('a');
-    anchor?.setAttribute('target', targetCell?.textContent?.trim());
     anchor?.classList.add('download-file');
-
     const href = anchor?.getAttribute('href');
     if (href?.startsWith('/-/media')) {
       anchor.setAttribute('href', `https://www.bangkokbank.com${href}`);
     }
 
-    const icon = document.createElement('span');
-    icon.className = 'icon-download';
-    anchor?.append(icon);
+    anchor.insertAdjacentHTML('beforeend', '<span class="icon-download"></span>');
 
     linkCell.className = 'download-item';
     moveInstrumentation(row, linkCell);
