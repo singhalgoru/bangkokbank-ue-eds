c1afa3205685bb87aabea7b8d634a725f56eb1e0
SuchitraYaraguudri12 <syaragudri@adobe.com>
2026-02-26 10:51:12 +0530

    testing inside cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,46 +1,19 @@
-import { createOptimizedPicture, loadCSS } from '../../scripts/aem.js';
-import { moveInstrumentation, decorateDownloadAnchor } from '../../scripts/scripts.js';
-
-function tryDecorateDownloadInCard(div) {
-  if (div.children.length !== 2) return false;
-  const linkCell = div.firstElementChild;
-  const targetCell = div.lastElementChild;
-  const anchor = linkCell?.querySelector('a');
-  if (!anchor) return false;
-  const targetValue = targetCell?.textContent?.trim() || '_self';
-  const downloadItem = document.createElement('div');
-  downloadItem.className = 'download-item';
-  moveInstrumentation(div, downloadItem);
-  downloadItem.append(anchor);
-  decorateDownloadAnchor(anchor, targetValue);
-  const bodyWrap = document.createElement('div');
-  bodyWrap.className = 'cards-card-body';
-  bodyWrap.append(downloadItem);
-  div.replaceWith(bodyWrap);
-  return true;
-}
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  let hasDownloadInCards = false;
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) {
-        div.className = 'cards-card-image';
-      } else {
-        div.className = 'cards-card-body';
-        if (tryDecorateDownloadInCard(div)) hasDownloadInCards = true;
-      }
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
     });
     ul.append(li);
   });
-  if (hasDownloadInCards) {
-    loadCSS(`${window.hlx.codeBasePath}/blocks/download/download.css`);
-  }
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
--- blocks/download/download.js
+++ blocks/download/download.js
@@ -1,10 +1,20 @@
-import { moveInstrumentation, decorateDownloadAnchor } from '../../scripts/scripts.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     const [linkCell, targetCell] = row.children;
     const anchor = linkCell?.querySelector('a');
-    decorateDownloadAnchor(anchor, targetCell?.textContent?.trim());
+    anchor?.setAttribute('target', targetCell?.textContent?.trim());
+    anchor?.classList.add('download-file');
+
+    const href = anchor?.getAttribute('href');
+    if (href?.startsWith('/-/media')) {
+      anchor.setAttribute('href', `https://www.bangkokbank.com${href}`);
+    }
+
+    const icon = document.createElement('span');
+    icon.className = 'icon-download';
+    anchor?.append(icon);
 
     linkCell.className = 'download-item';
     moveInstrumentation(row, linkCell);
